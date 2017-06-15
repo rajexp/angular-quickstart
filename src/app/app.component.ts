ce593f012607df7613d81735d07f7db12c661c7c
Rajan Chauhan <rajanchauhan11@gmail.com>
2017-06-15 07:11:09 +0530

    Angular 4 with @angular/material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  name = 'Angular';
+  appname = 'Quickstart';
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCheckboxModule, MdToolbarModule, MdIconModule, MdCardModule} from '@angular/material';
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, MdButtonModule, MdCheckboxModule, MdToolbarModule, MdIconModule, MdCardModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
